100f6ad34c13e782b2d12ed130e3c382dca0fdf1
Chatter Tech <reneekrom28@gmail.com>
2020-06-27 16:44:14 +0300

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCxRX1MSwkrlnLuMZc3N3xvlvymGcdMqhQ',
+    authDomain: 'kanban-2811.firebaseapp.com',
+    databaseURL: 'https://kanban-2811.firebaseio.com',
+    projectId: 'kanban-2811',
+    storageBucket: 'kanban-2811.appspot.com',
+    messagingSenderId: '991335523969',
+    appId: '1:991335523969:web:f5e07b224c7cb9d2100f80',
+    measurementId: 'G-4GR336VTXL'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCxRX1MSwkrlnLuMZc3N3xvlvymGcdMqhQ',
+    authDomain: 'kanban-2811.firebaseapp.com',
+    databaseURL: 'https://kanban-2811.firebaseio.com',
+    projectId: 'kanban-2811',
+    storageBucket: 'kanban-2811.appspot.com',
+    messagingSenderId: '991335523969',
+    appId: '1:991335523969:web:f5e07b224c7cb9d2100f80',
+    measurementId: 'G-4GR336VTXL'
+  }
 };
 
 /*
