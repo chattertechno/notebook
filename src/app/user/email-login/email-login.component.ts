7ba006aba94efbe6b30acd1d9200c6b96d38a405
Chatter Tech <reneekrom28@gmail.com>
2020-06-27 19:23:27 +0300

    THE BLACKLIST

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -15,53 +15,79 @@ export class EmailLoginComponent implements OnInit {
   form: FormGroup;
 
   type: 'login' | 'signup' | 'reset' = 'signup';
-  loading: false;
+  loading = false;
 
   serverMessage: string;
 
   constructor(private afAuth: AngularFireAuth, private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.minLength(6)]],
+      password: [
+        '',
+        [Validators.minLength(6), Validators.required]
+      ],
       passwordConfirm: ['', []]
-
     });
   }
 
-    changeType(val) {
-      this.type = val;
-    }
+  changeType(val) {
+    this.type = val;
+  }
 
-    get isLogin() {
-      return this.type === 'login';
-    }
+  get isLogin() {
+    return this.type === 'login';
+  }
 
-    get isSignup() {
-      return this.type === 'signup';
-    }
+  get isSignup() {
+    return this.type === 'signup';
+  }
 
-    get isPasswordReset() {
-      return this.type === 'reset';
-    }
+  get isPasswordReset() {
+    return this.type === 'reset';
+  }
 
-    get email() {
-      return this.form.get('email');
-    }
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
 
-    get password() {
-      return this.form.get('password');
-    }
+  get passwordConfirm() {
+    return this.form.get('passwordConfirm');
+  }
 
-    get passwordConfirm() {
-      return this.form.get('passwordConfirm');
+  get passwordDoesMatch() {
+    if (this.type !== 'signup') {
+      return true;
+    } else {
+      return this.password.value === this.passwordConfirm.value;
     }
+  }
 
-    get passwordDoesMatch() {
-      if (this.type !== 'signup') {
-        return true;
+  async onSubmit() {
+    this.loading = true;
+
+    const email = this.email.value;
+    const password = this.password.value;
+
+    try {
+      if (this.isLogin) {
+        await this.afAuth.signInWithEmailAndPassword(email, password);
       }
+      if (this.isSignup) {
+        await this.afAuth.createUserWithEmailAndPassword(email, password);
+      }
+      if (this.isPasswordReset) {
+        await this.afAuth.sendPasswordResetEmail(email);
+        this.serverMessage = 'Check your email';
+      }
+    } catch (err) {
+      this.serverMessage = err;
     }
 
+    this.loading = false;
+  }
 }
